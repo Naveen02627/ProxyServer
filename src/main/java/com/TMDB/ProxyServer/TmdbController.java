7cf06b7cd8b28e7fc3d6a303d5edb9abb9f7a74e
Naveen02627 <jangidnaveen321@gail.com>
2026-06-05 15:58:08 +0530

    Added test endpoint

--- src/main/java/com/TMDB/ProxyServer/TmdbController.java
+++ src/main/java/com/TMDB/ProxyServer/TmdbController.java
@@ -1,13 +1,12 @@
-package com.example.tmdbproxy.controller;
+package com.TMDB.ProxyServer;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.Map;
 
 @RestController
